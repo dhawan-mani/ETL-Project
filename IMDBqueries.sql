5af35d4586f71c90d896de53cc47e6005fd2b3f8
Aaron Eaker <aaroneaker@hotmail.com>
2020-09-03 15:45:19 -0400

    updated schema and queries with more questions answered

--- IMDBqueries.sql
+++ IMDBqueries.sql
@@ -1,7 +1,30 @@
+/*How many times actors appeared in movies in range of years*/
+select n.name, count(m.title)
+from names as n 
+inner join titles as t on n.imdb_name_id = t.imdb_name_id
+inner join movies as m on t.imdb_title_id = m.imdb_title_id
+where t.category = 'actor' and m.year_published > 2015 or t.category = 'actress' and m.year_published = 2015
+group by n.name
+order by count(m.title) desc;
+
+
+/*Count times has Tom Cruise been in movies since 2010*/
+select n.name, count(m.title)
+from names as n 
+inner join titles as t on n.imdb_name_id = t.imdb_name_id
+inner join movies as m on t.imdb_title_id = m.imdb_title_id
+inner join movie_demo as md on md.imdb_title_id = m.imdb_title_id
+where n.name like '%Tom Cruise%' and m.year_published > 2010  or
+n.name like '%Tom Cruise%' and m.year_published > 2010 
+group by n.name
+order by count(m.title) desc;
+
+/*where t.category = 'actor'*/ /*and md.genre = 'Action'*/
+/*or t.category = 'actress'*/ /*and md.genre = 'Action'*/
+
 /*How many movies did Humphrey Bogart act in?*/
 select count(title) from movies
 where actors LIKE '%Humphrey Bogart%';
-
 
 /*50 Highest Rated Movies with more than 2000 votes.*/
 select m.title, m.director, max(r.weighted_average_vote )
@@ -10,20 +33,13 @@ inner join ratings as r on m.imdb_title_id = r.imdb_title_id
 where r.total_votes >= 2000
 group by m.title, m.director
 order by max(r.weighted_average_vote) desc limit 50;
-/*check this out*/
 
-
-select movie.title, movie.genre, ratings.weighted_average_vote, movie.votes /* which movie had the highest rating regardless of the genre. */
-from movie join ratings on(movie.imdb_title_id = ratings.imdb_title_id)
-where movie.votes > 20000 Order by ratings.weighted_average_vote desc, movie.votes desc;
-
-
-
-/*Most active writer*/
-select writer, count(writer) 
+/*Most active director*/
+select director, count(director) 
 from movies
-group by writer
-order by count(writer) desc limit 1;
+group by director
+order by count(director) desc limit 1;
+
 
 /*top 10 years with most movies made*/
 select year_published, count(year_published)
@@ -46,11 +62,65 @@ select title, director, writer, actors
 from movies
 where director like writer and director like actors;
 
-/*both genders, age 30 ratings for movie genre */
-select movie_demo.genre, avg(ratings.allgenders_30age_avg_vote), count(ratings.allgenders_30age_avg_vote)
+/*both genders, age 18 ratings for movie genre */
+select movie_demo.genre, avg(ratings.allgenders_18age_avg_vote) as "Average Rating From All Genders 18 Age Group", 
+count(movie_demo.imdb_title_id) as "Number of Films in Genre",
+sum (ratings.total_votes) as "Total Votes for Genre"
 from ratings inner join movie_demo on ratings.imdb_title_id=movie_demo.imdb_title_id
 where movie_demo.votes >= 600
 group by movie_demo.genre
-order by count(ratings.allgenders_30age_avg_vote)desc, avg(ratings.allgenders_30age_avg_vote) desc;
+order by avg(ratings.allgenders_18age_avg_vote) desc, count(ratings.allgenders_18age_avg_vote)desc;
+
+/*both genders, age 30 ratings for movie genre */
+select movie_demo.genre, avg(ratings.allgenders_30age_avg_vote) as "Average Rating From All Genders 30 Age Group", 
+count(ratings.allgenders_30age_avg_vote) as "Number of Films in Genre",
+sum (ratings.total_votes) as "Total Votes for Genre"
+from ratings inner join movie_demo on ratings.imdb_title_id=movie_demo.imdb_title_id
+where movie_demo.votes >= 600
+group by movie_demo.genre
+order by avg(ratings.allgenders_30age_avg_vote) desc, count(ratings.allgenders_30age_avg_vote)desc;
+
+/*both genders, age 45 ratings for movie genre */
+select movie_demo.genre, avg(ratings.allgenders_45age_avg_vote) as "Average Rating From All Genders 45 Age Group", 
+count(ratings.allgenders_45age_avg_vote) as "Number of Films in Genre",
+sum (ratings.total_votes) as "Total Votes for Genre"
+from ratings inner join movie_demo on ratings.imdb_title_id=movie_demo.imdb_title_id
+where movie_demo.votes >= 600
+group by movie_demo.genre
+order by avg(ratings.allgenders_45age_avg_vote) desc, count(ratings.allgenders_45age_avg_vote)desc;
+
+/*Male All Ages Votes*/
+select movie_demo.genre, avg(ratings.males_allages_avg_vote) as "Average Rating From Males All Age Group", 
+count(ratings.males_allages_avg_vote) as "Number of Films in Genre",
+sum (ratings.total_votes) as "Total Votes for Genre"
+from ratings inner join movie_demo on ratings.imdb_title_id=movie_demo.imdb_title_id
+where movie_demo.votes >= 600
+group by movie_demo.genre
+order by avg(ratings.males_allages_avg_vote) desc, count(ratings.males_allages_avg_vote)desc;
+
+/*Female All Ages Votes*/
+select movie_demo.genre, avg(ratings.females_allages_avg_vote) as "Average Rating From Females All Age Group", 
+count(ratings.females_allages_avg_vote) as "Number of Films in Genre",
+sum (ratings.total_votes) as "Total Votes for Genre"
+from ratings inner join movie_demo on ratings.imdb_title_id=movie_demo.imdb_title_id
+where movie_demo.votes >= 600
+group by movie_demo.genre
+order by avg(ratings.females_allages_avg_vote) desc, count(ratings.females_allages_avg_vote)desc;
+
+
+
+
+
+/*Number of People in each Job Category */
+select category, count(category)
+from titles
+group by category;
+
+/*Number of people and jobs related to the Movie "Batman Begins"*/
+select movies.title, titles.category, count(titles.category)
+from movies
+inner join titles on movies.imdb_title_id=titles.imdb_title_id
+where movies.title like '%Batman Begins%'
+group by movies.title, titles.category;
 
 
--- SQLschema.sql
+++ SQLschema.sql
@@ -13,13 +13,19 @@ create table movies(
 
 create table names(
 	imdb_name_id varchar,
-	name varchar); 
+	name varchar,
+	primary key(imdb_name_id)); 
 	
 create table titles(
 	imdb_title_id varchar,
 	imdb_name_id varchar,
-	category varchar
-);
+	category varchar,
+	primary key(imdb_name_id, imdb_title_id),
+	FOREIGN KEY (imdb_name_id) REFERENCES names(imdb_name_id),
+	FOREIGN KEY (imdb_title_id) REFERENCES movies(imdb_title_id));
+
+
+
 
 /*drop table ratings;*/
 create table ratings(
@@ -32,12 +38,24 @@ create table ratings(
 	males_allages_avg_vote float,
 	females_allages_avg_vote float,
 	us_voters_votes float,
-	non_us_voters_votes float); 
+	non_us_voters_votes float,
+	primary key(imdb_title_id),
+	FOREIGN KEY (imdb_title_id) REFERENCES movies(imdb_title_id));
+ 
 
 create table movie_demographic(
 	imdb_title_id varchar,
 	title varchar,
 	genre varchar,
 	votes int,
-	Year_Published int
-)
+	Year_Published int,
+	PRIMARY KEY (imdb_title_id),
+	FOREIGN KEY (imdb_title_id) REFERENCES movies(imdb_title_id)
+);
+
+
+create table profession(
+	profession varchar,
+	profession_id varchar (4)
+	primary key (profession_id)
+);
